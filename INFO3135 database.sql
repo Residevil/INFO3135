3ae151708ef31d4a2bb31cdb2e2853bbc8731ad5
Residevil <43865235+Residevil@users.noreply.github.com>
2020-11-22 18:31:37 -0800

    Update INFO3135 database.sql

--- INFO3135 database.sql
+++ INFO3135 database.sql
@@ -38,8 +38,9 @@ CREATE TABLE violator (
 CREATE TABLE violation (
     violation_id INT(11) NOT NULL AUTO INCREMENT,
     violation_number INT(50) NOT NULL,
-    violation_date DATE(20),
-    fine_amount FLOAT(11),
+    violation_date DATE(20) NOT NULL,
+    fine_amount FLOAT(11) NOT NULL,
+    fine_due_date DATE(20) NOT NULL,
     violator_id INT(11),
     violation_type_id INT(11),
     license_plate VARCHAR(11),
