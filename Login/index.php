94af9cde923bd6c5d4f382afbce7e33ce74cb7cf
Widangi <39718916+Widangi@users.noreply.github.com>
2020-11-22 16:17:08 -0800

    Added Violations HTML pages

---
+++ Login/index.php
@@ -0,0 +1,16 @@
+<?php include'../view/header.php'; ?>
+
+
+	<body>
+		<form METHOD="post">
+			<div>
+				<input type="text" placeholder="Usesrname">
+				<input type="password" placeholder="Password">
+			</div>
+		
+			<div>
+				<button type="submit" name="login" class="login_b">Login</button>
+			</div>
+		</form>
+	</body>
+</html>
