2c94a1386541db1426884a66a47ccffb1f869301
Garry Bassi <mrgarrybassi@gmail.com>
2020-11-23 22:49:12 -0800

    Revert "Merge pull request #2 from mrgarrybassi/Development"
    This reverts commit c89c85602456e2a498dab7ef5c03df5a2ace1bd6, reversing
    changes made to 914cfea246911a28f047add54202aa50491cc516.

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php include 'view/header.php'; ?>
-    <body>
-    <div id="main">
-    <h1 class="top">Menu</h1>
-    <p><a href="">Search for a Violation</a></p>
-    <p><a href="product_catalog">Product Catalog</a></p>
-</div>
-<?php include 'view/footer.php'; ?>
--- view/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <p class="copyright">
-        &copy; <?php echo date("Y"); ?> City of XYZ - Traffic Ticketing System
-    </p>
-</footer>
-</body>
-</html>
