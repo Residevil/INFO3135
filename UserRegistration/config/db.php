f528fdaa1a0f02b118e8a3e8fbd95c3e2a5a0028
Residevil <43865235+Residevil@users.noreply.github.com>
2020-12-17 17:35:17 -0800

    Update db.php

--- UserRegistration/config/db.php
+++ UserRegistration/config/db.php
@@ -10,37 +10,46 @@ require 'constants.php'; //use to contain sensitive data
         
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS) or die ('Database error: ' . $conn->connect_error);
-$db = "CREATE DATABASE INFO3135";
 
-// sql to create table
-$employees = "CREATE TABLE employees (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    username VARCHAR(20) NOT NULL , 
-    email VARCHAR(50) NOT NULL UNIQUE, 
-    verified TINYINT(4) NOT NULL, 
-    token VARCHAR(100) NOT NULL, 
-    password VARCHAR(100) NOT NULL,
-    employee_type_id INT(11) NOT NULL FOREIGN KEY REFERENCE employee_type (id)
-)";
+$db_selected = $conn->select_db( DB_NAME );
+if(!$db_selected) {
+    $db = "CREATE DATABASE IF NOT EXISTS INFO 2413";
+    $conn->query($db);
 
-$employee_type = "CREATE TABLE employee_type (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,  
-    employee_type VARCHAT(50) NOT NULL
-)";
+    $conn->select_db( DB_NAME );
 
-$ticket = "CREATE TABLE ticket (
-    ticket_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    ticket_num INT(20) NOT NULL ,
-    Car_license VARCHAR(10) NOT NULL UNIQUE, 
-    ticket_type VARCHAT(50) NOT NULL
-)";
+    if ($conn->query($db) === TRUE) {
+            // sql to create table.
+        $employees = "CREATE TABLE employees (
+            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+            username VARCHAR(20) NOT NULL , 
+            email VARCHAR(50) NOT NULL UNIQUE, 
+            verified TINYINT(4) NOT NULL, 
+            token VARCHAR(100) NOT NULL, 
+            password VARCHAR(100) NOT NULL,
+            employee_type_id INT(11) NOT NULL FOREIGN KEY REFERENCE employee_type (id)
+        )";
 
-$ticket_type = "CREATE TABLE ticket_type (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    ticket_type VARCHAT(50) NOT NULL,
-    file_No INT(50) NOT NULL
-)";
+        $employee_type = "CREATE TABLE employee_type (
+            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,  
+            employee_type VARCHAT(50) NOT NULL
+        )";
 
+        $ticket = "CREATE TABLE ticket (
+            ticket_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+            ticket_num INT(20) NOT NULL ,
+            Car_license VARCHAR(10) NOT NULL UNIQUE, 
+            ticket_type VARCHAT(50) NOT NULL
+        )";
+
+        $ticket_type = "CREATE TABLE ticket_type (
+            id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+            ticket_type VARCHAT(50) NOT NULL,
+            file_No INT(50) NOT NULL
+        )";
+    }
+    else { echo "Error creating table: " . $conn->error;}
+}
 
 
 ?>
